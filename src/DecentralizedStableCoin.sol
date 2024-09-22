bffcf823b9d77219f9e5ac1f18e69def3ad6ff5a
ybtuti <samuelmsuu@gmail.com>
2024-09-22 13:57:21 +0300

    Feat: Add DecentralizedStableCoin contract layout

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+//Layout of contract:
+// Version
+//Imports
+//Errors
+//Interfaces, libraries, contracts
+// type declations
+// State variables
+// Events
+// Modofiers
+//Functions
+
+//Layout of Functions;
+// Constructors
+// Receive function
+// Fallback Function
+// External 
+// Public
+// Internal
+// Private
+// View & Pure functions
