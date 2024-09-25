c33df0954a1f3308ac42d91c10b312e630180d47
ybtuti <samuelmsuu@gmail.com>
2024-09-25 10:32:31 +0300

    refactor: Update DeployDSC contract to return HelperConfig instance
    This commit updates the DeployDSC contract to return an instance of the HelperConfig contract along with the DecentralizedStableCoin and DSCEngine instances. This change allows for easier access to the HelperConfig contract for configuring network settings.
    
    Refactor the `run` function in the DeployDSC contract to return `(DecentralizedStableCoin, DSCEngine, HelperConfig)` instead of just `(DecentralizedStableCoin, DSCEngine)`.
    
    No issues or tags are associated with this commit.

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -7,11 +7,11 @@ import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
-contract DeployDsc is Script {
+contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
-    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
         HelperConfig config = new HelperConfig();
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             config.activeNetworkConfig();
@@ -24,6 +24,6 @@ contract DeployDsc is Script {
         DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
         dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
-        return (dsc, engine);
+        return (dsc, engine, config);
     }
 }
