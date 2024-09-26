22f3da06f822bab51bdb9bff2c26b8fbd4bfc951
ybtuti <samuelmsuu@gmail.com>
2024-09-26 14:18:09 +0300

    refactor: Add test for checking protocol's collateral value against total supply

---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: MIT
+
+// Have our invariant aka properties
+//what are our invariants?
+
+// 1. Total value of DSC should be less than the total value of the collateral
+
+// 2. Getter view functions should never revert <- evergreen invariants
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        // targetContract(address(dsce));
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol
+        // compare it tothe debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log("Weth Value:", wethValue);
+        console.log("Wbtc Value:", wbtcValue);
+        console.log("Total Supply:", totalSupply);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
