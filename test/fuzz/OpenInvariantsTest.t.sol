16a745be43f2f0cb399275b6bb64dc60b911b487
ybtuti <samuelmsuu@gmail.com>
2024-09-26 14:20:32 +0300

    refactor: Remove commented out code in OpenInvariantsTest

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,51 +1,51 @@
-//SPDX-License-Identifier: MIT
+// //SPDX-License-Identifier: MIT
 
-// Have our invariant aka properties
-//what are our invariants?
+// // Have our invariant aka properties
+// //what are our invariants?
 
-// 1. Total value of DSC should be less than the total value of the collateral
+// // 1. Total value of DSC should be less than the total value of the collateral
 
-// 2. Getter view functions should never revert <- evergreen invariants
+// // 2. Getter view functions should never revert <- evergreen invariants
 
-pragma solidity ^0.8.18;
+// pragma solidity ^0.8.18;
 
-import {Test, console} from "forge-std/Test.sol";
-import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DeployDSC} from "script/DeployDSC.s.sol";
-import {DSCEngine} from "src/DSCEngine.sol";
-import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {Test, console} from "forge-std/Test.sol";
+// import {StdInvariant} from "forge-std/StdInvariant.sol";
+// import {DeployDSC} from "script/DeployDSC.s.sol";
+// import {DSCEngine} from "src/DSCEngine.sol";
+// import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+// import {HelperConfig} from "script/HelperConfig.s.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract InvariantsTest is StdInvariant, Test {
-    DeployDSC deployer;
-    DSCEngine dsce;
-    DecentralizedStableCoin dsc;
-    HelperConfig config;
-    address weth;
-    address wbtc;
+// contract OpenInvariantsTest is StdInvariant, Test {
+//     DeployDSC deployer;
+//     DSCEngine dsce;
+//     DecentralizedStableCoin dsc;
+//     HelperConfig config;
+//     address weth;
+//     address wbtc;
 
-    function setUp() external {
-        deployer = new DeployDSC();
-        (dsc, dsce, config) = deployer.run();
-        (,, weth, wbtc,) = config.activeNetworkConfig();
-        targetContract(address(dsce));
-    }
+//     function setUp() external {
+//         deployer = new DeployDSC();
+//         (dsc, dsce, config) = deployer.run();
+//         (,, weth, wbtc,) = config.activeNetworkConfig();
+//         targetContract(address(dsce));
+//     }
 
-    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
-        // get the value of all the collateral in the protocol
-        // compare it tothe debt (dsc)
-        uint256 totalSupply = dsc.totalSupply();
-        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
-        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+//     function invariant_protocolMustHaveMoreValueThanTotalSupplys() public view {
+//         // get the value of all the collateral in the protocol
+//         // compare it tothe debt (dsc)
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+//         uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
 
-        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
-        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+//         uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+//         uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
 
-        console.log("Weth Value:", wethValue);
-        console.log("Wbtc Value:", wbtcValue);
-        console.log("Total Supply:", totalSupply);
+//         console.log("Weth Value:", wethValue);
+//         console.log("Wbtc Value:", wbtcValue);
+//         console.log("Total Supply:", totalSupply);
 
-        assert(wethValue + wbtcValue >= totalSupply);
-    }
-}
+//         assert(wethValue + wbtcValue >= totalSupply);
+//     }
+// }
